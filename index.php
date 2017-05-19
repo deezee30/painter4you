8fb831876ed86ecef08bf8b2ede27db6fcdead37
Maulss <maulss@gmx.com>
2017-05-20 00:38:33 +0100

    Disable shrinking on extreme devices

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ function get_ip() {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
     <meta name="description" content="Paul's affordable and high quality painting and decorating services are offered here" />
     <meta name="keywords" content="galway, paint, painter, painting, decorator, decorating, cheap, affordable, high, quality, fast, efficient, gatis, pauls, paul, painterforyou">
 
