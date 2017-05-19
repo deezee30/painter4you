e0f566c0af8ec0e523ba394a566e549ba5ef6f2a
Maulss <maulss@gmx.com>
2017-05-19 19:01:10 +0100

    Change services layout, add double columns to box

--- components/services.php
+++ components/services.php
@@ -1,36 +1,53 @@
-<div class="row">
-    <div class="col-sm-3">
-        <div class="well well-sm">
-            <div class="text-center">
-                <h4>Skills Offered</h4>
-            </div>
-            <ul>
-                <li>Interior House Painting</li>
-                <li>Exterior House Painting</li>
-                <li>Apartment Painting</li>
-                <li>Wallpaper Hanging</li>
-                <li>Wallpaper Removing</li>
-                <li>Staining</li>
-                <li>Wall Repair</li>
-                <li>Wall Murals</li>
-                <li>Faux Painting</li>
-                <li>Kitchen Cabinets Painting</li>
-                <li>Wood Floor Painting</li>
-                <li>Deck Staining</li>
-                <li>Spray Painting</li>
-                <li>Graffity Removal</li>
-                <li>And more!</li>
-            </ul>
-        </div>
-    </div>
+<div class="box">
+    <div class="row">
 
-    <div class="col-sm-9">
-        <div class="box">
+        <!-- left -->
+        <div class="col-sm-3">
             <div class="box-content">
+                <!-- title -->
+                <div class="text-center">
+                    <h3>Skills Offered</h3>
+                </div>
+                <!-- /title -->
+
+                <!-- skills offered list -->
+                <ul class="tick">
+                    <li>Interior House Painting</li>
+                    <li>Exterior House Painting</li>
+                    <li>Apartment Painting</li>
+                    <li>Wallpaper Hanging</li>
+                    <li>Wallpaper Removing</li>
+                    <li>Staining</li>
+                    <li>Wall Repair</li>
+                    <li>Wall Murals</li>
+                    <li>Faux Painting</li>
+                    <li>Kitchen Cabinets Painting</li>
+                    <li>Wood Floor Painting</li>
+                    <li>Deck Staining</li>
+                    <li>Spray Painting</li>
+                    <li>Graffity Removal</li>
+                    <li>And more!</li>
+                </ul>
+                <!-- /skills offered list -->
+
+                <!-- bucket image -->
+                <br />
+                <img class="center-block" height=100 src="../img/bucket.png" />
+            </div>
+        </div>
+        <!-- /left -->
+
+        <!-- right -->
+        <div class="vertical col-sm-9">
+            <div class="box-content">
+                <!-- title -->
                 <div class="text-center">
                     <h3>So Why Choose Me?</h3>
                 </div>
+                <!-- /title -->
+
                 <div class="row">
+                    <!-- left -->
                     <div class="col-sm-6">
                         <ul>
                             <li><h4>QUALITY</h4> Quality of work is very important for us.</li>
@@ -40,6 +57,9 @@
                             <li><h4>COMMITMENT</h4> I promise to work on consecutive days until the job is done completely.</li>
                         </ul>
                     </div>
+                    <!-- /left -->
+
+                    <!-- right -->
                     <div class="col-sm-6">
                         <ul>
                             <li><h4>HONESTY</h4> I am sure you will find a trustworthy people in us. When I say we will be there at 8:30 we will. When I say we will finish the work in 4-5 days, we will do our best to finish it in 4 days. When I say the price is such and such, it is.</li>
@@ -47,8 +67,10 @@
                             <li><h4>CARE</h4> Your property and values in it are in the good hands. We are taking special care to protect your possessions during our work, to be able to leave the property as we found it.</li>
                         </ul>
                     </div>
+                    <!-- /right -->
                 </div>
             </div>
         </div>
+        <!-- /right -->
     </div>
 </div>
